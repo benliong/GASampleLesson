ee17c92d4c68363f7c7e3e1404708b49785d3a4c
Ben Liong <ben.liong@bindo.com>
2015-03-24 01:14:28 +0800

    Initial Commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var temperatureLabel: UILabel?
+    @IBOutlet weak var getTemperatureButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func getCurrentTemperature(sender: UIButton) {
+        <# General Assembly, Put Networking Code Here #>
+        
+//        let url = NSURL(string: "http://pixelicious.com.hk/weather.html")
+//        
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+//            let content = NSString(data: data, encoding: NSUTF8StringEncoding)
+//            println(content!)
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                self.temperatureLabel!.text = content
+//            })
+//        }
+//        
+//        task.resume()
     }
 
 
